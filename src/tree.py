e509e2a3cf798cc99554828b07596f68eb8213c8
Ryan Zhang <ryan.zhang3@student.tdsb.on.ca>
2023-08-01 22:06:12 -0400

    Fixed basename in path

--- src/tree.py
+++ src/tree.py
@@ -27,7 +27,6 @@ class Tree:
         self.walk = os.walk(self.dir)
     
     def create_tree(self) -> None:
-        root_path = os.path.abspath(self.dir)
 
         for folder, subfolders, files in self.walk:
             depth : int = self.get_directory_depth(folder) - self.get_directory_depth(self.dir)
@@ -37,13 +36,13 @@ class Tree:
             if self.include_folders:
                 path = os.path.abspath(folder)
 
-                if path != root_path:
+                if folder != self.dir:
                     if not self.include_abspath:
-                        path = self.getDifference(path, root_path)
+                        path = self.get_basename(path)
 
                     self.tree.append(self.divider*(depth) + "├----" + path)
                     self.depths.append(depth)
-                    self.addRootLines(depth)
+                    self.add_root_lines(depth)
 
             # files in folder
             if self.include_files:
@@ -52,14 +51,12 @@ class Tree:
                         path = os.path.abspath(f)
 
                         if not self.include_abspath:
-                            path = self.getDifference(path, root_path)
+                            path = self.get_basename(path)
 
                         self.tree.append(self.divider*(depth+1) + "├----" + path)
                         self.depths.append(depth+1)
                     except UnicodeEncodeError:
                         self.tree.append("???")
-                
-                self.addRootLines(depth+1)
 
     def print_tree(self) -> None:
         for line in self.tree:
@@ -68,7 +65,7 @@ class Tree:
     def get_directory_depth(self, path : str) -> int:
         return path.count(os.path.sep)
 
-    def addRootLines(self, depth : int) -> None:
+    def add_root_lines(self, depth : int) -> None:
         # add | to every directory in self.tree at index depth*5
         for i in range(len(self.tree)-2, 0, -1):
             try:
@@ -79,16 +76,6 @@ class Tree:
             except IndexError:
                 continue
     
-    def getDifference(self, full_path, reference_path):
-        # gets the remaining path of full_path - reference_path
-        full_path = os.path.normpath(full_path)
-        reference_path = os.path.normpath(reference_path)
-
-        # Check if the reference_path is a prefix of the full_path
-        if not full_path.startswith(reference_path):
-            raise ValueError("The reference_path is not a prefix of the full_path")
-
-        # Extract the remaining part of the full_path
-        remaining_path = full_path[len(reference_path):].strip(os.path.sep)
-
-        return remaining_path
+    def get_basename(self, path):
+        # gets the last part of path
+        return os.path.basename(path)
